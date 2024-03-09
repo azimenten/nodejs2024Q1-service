55db49a2233a5c22780ecd76b602004fdb4419e8
Azimkhan93 <aabdulsatarov@gmail.com>
2024-03-09 05:13:01 +0500

    feat: add resources for albums, artists, favourites and tracks

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ArtistsModule } from './artists/artists.module';
+import { TracksModule } from './tracks/tracks.module';
+import { AlbumsModule } from './albums/albums.module';
+import { FavouritesModule } from './favourites/favourites.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ArtistsModule, TracksModule, AlbumsModule, FavouritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
