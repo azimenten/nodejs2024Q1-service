e95aab6c97f08cd1f1abb14a1c266e7bd8c7f9c4
Azimkhan93 <aabdulsatarov@gmail.com>
2024-03-14 03:45:14 +0500

    refactor: remove console log

--- test/favorites.e2e.spec.ts
+++ test/favorites.e2e.spec.ts
@@ -185,7 +185,6 @@ describe('Favorites (e2e)', () => {
       const trackSearchRes = responseAfterDeletion.body.tracks.find(
         (track) => track.id === trackId,
       );
-      console.log(responseAfterDeletion.body);
       expect(artistSearchRes).toBeUndefined();
       expect(albumSearchRes).toBeUndefined();
       expect(trackSearchRes).toBeUndefined();
